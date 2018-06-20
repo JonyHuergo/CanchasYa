4a5eeb75e051c163ae993c8b57fe3ed5597b3ac4
JonyHuergo <jh2000@hotmail.com.ar>
2018-06-20 19:46:45 -0300

    Add files via upload

---
+++ objetos/SQLdb.php
@@ -0,0 +1,15 @@
+<?php
+
+  class SQLdb extends Db{
+
+
+    private $PDO:
+    const $OPT = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+    public function __construct($dns, $user, $password){
+      $this->PDO = new PDO($dns, $user, $password, $OPT);
+    }
+
+  }
+
+ ?>

adfcea8b74993745ae010743a494408c696abd0a
JonyHuergo <jh2000@hotmail.com.ar>
2018-06-21 18:40:59 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -4,9 +4,8 @@ if(isset($_SESSION["registrado"])){
   header("Location: paginaPrincipal.html");
 }
 // require_once "funciones/validaciones.php";
-require_once "objetos/validaciones.php";
-require_once "funciones/funcionesUsuarios.php";
-require_once "objetos/JsonDb.php";
+require_once "clases/validaciones.php";
+require_once "clases/JsonDb.php";
 
 $errores = [
   "username" => [],
